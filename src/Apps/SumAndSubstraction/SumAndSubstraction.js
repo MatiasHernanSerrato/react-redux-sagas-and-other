907979599f5aa2876b30ff152487399f59a1751f
Matt <matias.serrato@intive-fdv.com>
2020-09-27 16:06:52 -0300

    Comment moved

--- src/Apps/SumAndSubstraction/SumAndSubstraction.js
+++ src/Apps/SumAndSubstraction/SumAndSubstraction.js
@@ -4,13 +4,13 @@ import CustomButton from '../../Components/shared/CustomButton';
 
 const SumAndSubstraction = () => {
   const [count, setCounter] = useState(0)
-
+  //the last button was added in order to see how defaults props works
   return (
     <Fragment>
       <CustomButton text={'Sum'} onClickFn={() => setCounter(count + 1)} />
       <CustomButton text={'Substraction'} onClickFn={() => setCounter(count - 1)} />
       RESULT: {count}
-      <CustomButton></CustomButton> //this buttons was added to see how defaults props works
+      <CustomButton></CustomButton>
     </Fragment>
   );
 }
